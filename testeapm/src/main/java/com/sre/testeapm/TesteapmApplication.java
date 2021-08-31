f444635c3540a597abef74b6d86fa3c6c7745995
pehgomess <pehgomess@gmail.com>
2021-08-30 23:31:23 -0300

    first - Spring Boot + Docs

---
+++ testeapm/src/main/java/com/sre/testeapm/TesteapmApplication.java
@@ -0,0 +1,13 @@
+package com.sre.testeapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteapmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteapmApplication.class, args);
+	}
+
+}
---
+++ testeapm/src/test/java/com/sre/testeapm/TesteapmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sre.testeapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteapmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
